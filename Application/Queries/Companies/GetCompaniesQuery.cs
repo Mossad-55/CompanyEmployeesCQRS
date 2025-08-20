56df19d0fd61076372192203a74ca36d3d5a8fca
Mossad <mosad55522@gmail.com>
2025-08-20 17:19:31 +0300

    Implemented the GetCompaniesHandler

--- Application/Queries/Companies/GetCompaniesQuery.cs
+++ Application/Queries/Companies/GetCompaniesQuery.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Shared.DataTransferObjects;
+using Shared.RequestFeatures;
 
 namespace Application.Queries.Companies;
 
-public sealed record GetCompaniesQuery(bool trackChanges) : IRequest<IEnumerable<CompanyDto>>;
+public sealed record GetCompaniesQuery(CompanyParameters companyParameters, bool trackChanges) : IRequest<IEnumerable<CompanyDto>>;
