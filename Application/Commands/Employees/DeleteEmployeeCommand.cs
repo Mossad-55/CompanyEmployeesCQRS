9d8905ff9c8ec5acc36f12e1cb9f4562a03f6dd5
Mossad <mosad55522@gmail.com>
2026-07-02 20:06:39 +0300

    Added DeleteEmployee Handler and Command

---
+++ Application/Commands/Employees/DeleteEmployeeCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Application.Commands.Employees;
+
+public sealed record DeleteEmployeeCommand(Guid CompanyId, Guid Id, bool CompTrackChanges, bool EmpTrackChanges) : IRequest;
---
+++ Application/Handlers/Employees/DeleteEmployeeHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Commands.Employees;
+using Contracts;
+using Entities.Exceptions;
+using MediatR;
+
+namespace Application.Handlers.Employees;
+
+internal sealed class DeleteEmployeeHandler : IRequestHandler<DeleteEmployeeCommand>
+{
+    private readonly IRepositoryManager _repository;
+
+    public DeleteEmployeeHandler(IRepositoryManager repository) => _repository = repository;
+
+    public async Task Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var company = await _repository.Company.GetCompanyAsync(request.CompanyId, request.CompTrackChanges);
+        if (company is null)
+            throw new CompanyNotFoundException(request.CompanyId);
+
+        var employeeEntity = await _repository.Employee.GetEmployeeAsync(request.CompanyId, request.Id, request.EmpTrackChanges);
+        if (employeeEntity is null)
+            throw new EmployeeNotFoundException(request.Id);
+
+        _repository.Employee.DeleteEmployee(employeeEntity);
+
+        await _repository.SaveAsync();
+    }
+}
