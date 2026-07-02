a9768d9e8f5fda1149aa4a0da3a35d7c0b9fae9b
Mossad <mosad55522@gmail.com>
2026-07-02 19:13:34 +0300

    Added CreateEmployee Handler

---
+++ Application/Commands/Employees/CreateEmployeeCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Shared.DataTransferObjects;
+
+namespace Application.Commands.Employees;
+
+public sealed record CreateEmployeeCommand(Guid CompanyId, EmployeeForCreationDto Employee, bool CompTrackChanges, bool EmpTrackChanges) : IRequest;
---
+++ Application/Handlers/Employees/CreateEmployeeHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Commands.Employees;
+using AutoMapper;
+using Contracts;
+using Entities.Exceptions;
+using Entities.Models;
+using MediatR;
+
+namespace Application.Handlers.Employees;
+
+internal sealed class CreateEmployeeHandler : IRequestHandler<CreateEmployeeCommand>
+{
+    private readonly IRepositoryManager _repository;
+    private readonly IMapper _mapper;
+
+    public CreateEmployeeHandler(IRepositoryManager repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var company = await _repository.Company.GetCompanyAsync(request.CompanyId, request.CompTrackChanges);
+        if(company is null)
+            throw new CompanyNotFoundException(request.CompanyId);
+
+        var employeeEntity = _mapper.Map<Employee>(request.Employee);
+
+        _repository.Employee.CreateEmployeeForCompany(request.CompanyId, employeeEntity);
+
+        await _repository.SaveAsync();
+    }
+}
