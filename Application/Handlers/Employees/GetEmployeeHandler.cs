bdae127cbbcdc6f03733a0f7bcf5bdcd38f1a40a
Mossad <mosad55522@gmail.com>
2026-07-02 19:06:30 +0300

    Added GetEmployee Handler

---
+++ Application/Handlers/Employees/GetEmployeeHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Queries.Employees;
+using AutoMapper;
+using Contracts;
+using Entities.Exceptions;
+using MediatR;
+using Shared.DataTransferObjects;
+
+namespace Application.Handlers.Employees;
+
+internal sealed class GetEmployeeHandler : IRequestHandler<GetEmployeeQuery, EmployeeDto>
+{
+    private readonly IRepositoryManager _repository;
+    private readonly IMapper _mapper;
+
+    public GetEmployeeHandler(IRepositoryManager repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    async Task<EmployeeDto> IRequestHandler<GetEmployeeQuery, EmployeeDto>.Handle(GetEmployeeQuery request, CancellationToken cancellationToken)
+    {
+        var company = await _repository.Company.GetCompanyAsync(request.CompanyId, request.CompTrackChanges);
+        if (company is null)
+            throw new CompanyNotFoundException(request.CompanyId);
+
+        var employeeEntity = await _repository.Employee.GetEmployeeAsync(request.CompanyId, request.Id, request.EmpTrackChanges);
+        if (employeeEntity is null)
+            throw new EmployeeNotFoundException(request.Id);
+
+        var employeeDto = _mapper.Map<EmployeeDto>(employeeEntity);
+
+        return employeeDto;
+    }
+}
---
+++ Application/Queries/Employees/GetEmployeeQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Shared.DataTransferObjects;
+
+namespace Application.Queries.Employees;
+
+public sealed record GetEmployeeQuery(Guid CompanyId, Guid Id, bool CompTrackChanges, bool EmpTrackChanges) : IRequest<EmployeeDto>;
