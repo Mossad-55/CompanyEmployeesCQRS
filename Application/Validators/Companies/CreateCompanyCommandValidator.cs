e28f393bf7284dbed4eabf06dcd5294e5b835648
Mossad <mosad55522@gmail.com>
2026-07-04 20:11:06 +0300

    Added Validator for CreateCompanyCommand

---
+++ Application/Validators/Companies/CreateCompanyCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using Application.Commands.Companies;
+using FluentValidation;
+
+namespace Application.Validators.Companies;
+
+public sealed class CreateCompanyCommandValidator : AbstractValidator<CreateCompanyCommand>
+{
+    public CreateCompanyCommandValidator()
+    {
+        RuleFor(c => c.Company.Name).NotEmpty().MaximumLength(30);
+
+        RuleFor(c => c.Company.Address).NotEmpty().MaximumLength(60);
+
+        RuleFor(c => c.Company.Country).NotEmpty().MaximumLength(5);
+    }
+}
