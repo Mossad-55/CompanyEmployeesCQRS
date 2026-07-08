0157544094c5bec6bf8e9a0e18353b5237d74969
Mossad <mosad55522@gmail.com>
2026-07-08 16:19:45 +0300

    Created Validtor for EmployeeUpdateCommand

---
+++ Application/Validators/Employees/UpdateEmployeeCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using Application.Commands.Employees;
+using FluentValidation;
+
+namespace Application.Validators.Employees;
+
+public sealed class UpdateEmployeeCommandValidator : AbstractValidator<UpdateEmployeeCommand>
+{
+    public UpdateEmployeeCommandValidator()
+    {
+        RuleFor(e => e.Employee.Name).NotEmpty().MaximumLength(30);
+
+        RuleFor(e => e.Employee.Age).NotEmpty().InclusiveBetween(18, 65);
+
+        RuleFor(e => e.Employee.Position).NotEmpty().MaximumLength(20);
+    }
+}
